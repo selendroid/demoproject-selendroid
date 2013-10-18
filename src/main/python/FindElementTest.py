1b2e1657c2ae7260d960b8e6928f8123ae94c7d3
Dominik Dary <ddary@ebay.com>
2013-10-18 08:42:09 +0200

    adding a simple python test

---
+++ src/main/python/FindElementTest.py
@@ -0,0 +1,31 @@
+'''
+@author: Dominik Dary
+'''
+import unittest
+from selenium import webdriver
+ 
+  
+  class FindElementTest(unittest.TestCase):
+     
+    def setUp(self):
+      desired_capabilities = {'aut': 'io.selendroid.testapp:0.6.0-SNAPSHOT'}
+       
+       self.driver = webdriver.Remote(
+           desired_capabilities=desired_capabilities,
+           command_executor="http://127.0.0.1:5555/wd/hub"
+           )
+       self.driver.implicitly_wait(30)
+        
+        def test_find_element_by_id(self):
+          self.driver.get('and-activity://io.selendroid.testapp.HomeScreenActivity')
+          self.assertTrue("and-activity://HomeScreenActivity" in self.driver.current_url)
+          my_text_field = self.driver.find_element_by_id('my_text_field')
+          my_text_field.send_keys('Hello Selendroid')
+           
+           self.assertTrue('Hello Selendroid' in my_text_field.text)
+            
+            def tearDown(self):
+              self.driver.quit()
+               
+               if __name__ == '__main__':
+                 unittest.main()
