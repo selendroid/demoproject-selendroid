d1f8959627d9e0d197917ee145e5d01e13772418
Dominik Dary <ddary@acm.org>
2013-12-25 11:01:58 +0100

    Adding sample for testing mobile web with selendroid.

---
+++ src/main/java/io/selendroid/demo/mobileweb/MobileWebTest.java
@@ -0,0 +1,76 @@
+/*
+ * Copyright 2012-2013 eBay Software Foundation and selendroid committers.
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package io.selendroid.demo.mobileweb;
+
+import io.selendroid.SelendroidCapabilities;
+import io.selendroid.SelendroidConfiguration;
+import io.selendroid.SelendroidDriver;
+import io.selendroid.SelendroidLauncher;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+public class MobileWebTest {
+  private SelendroidLauncher selendroidServer = null;
+  private WebDriver driver = null;
+
+  @Test
+  public void shouldSearchWithEbay() {
+    // And now use this to visit ebay
+    driver.get("http://m.ebay.de");
+
+    // Find the text input element by its id
+    WebElement element = driver.findElement(By.id("kw"));
+
+    // Enter something to search for
+    element.sendKeys("Nexus 5");
+
+    // Now submit the form. WebDriver will find the form for us from the element
+    element.submit();
+
+    // Check the title of the page
+    System.out.println("Page title is: " + driver.getTitle());
+    driver.quit();
+  }
+
+  @Before
+  public void startSelendroidServer() throws Exception {
+    if (selendroidServer != null) {
+      selendroidServer.stopSelendroid();
+    }
+    SelendroidConfiguration config = new SelendroidConfiguration();
+    
+    selendroidServer = new SelendroidLauncher(config);
+    selendroidServer.lauchSelendroid();
+
+    DesiredCapabilities caps = SelendroidCapabilities.android();
+
+    driver = new SelendroidDriver("http://localhost:5555/wd/hub", caps);
+  }
+
+  @After
+  public void stopSelendroidServer() {
+    if (driver != null) {
+      driver.quit();
+    }
+    if (selendroidServer != null) {
+      selendroidServer.stopSelendroid();
+    }
+  }
+}
